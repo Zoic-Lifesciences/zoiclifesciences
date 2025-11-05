3658eb3bba917e88f10e8d9d7b8bc8f292725945
Aditi Rawat <aditirawat@Aditis-MacBook-Air.local>
2025-11-05 21:39:11 +0530

    Improved Newly Launched

--- app/components/NewlyLaunched.jsx
+++ app/components/NewlyLaunched.jsx
@@ -8,42 +8,42 @@ export default function NewlyLaunched() {
       name: "Hello Foot Care Cream",
       price: "INR. 249/-",
       desc: "Overnight Moisturising, Softens & Heals Dry Feet",
-      image: "/footcream.jpg",
+      image: "/footcream.jpeg",
     },
     {
       id: 2,
       name: "Hello Sunscreen",
       price: "INR. 349/-",
       desc: "Broad Spectrum, Protects from UV, Blue Light",
-      image: "/sunscreen.jpg",
+      image: "/suncreen.jpeg",
     },
     {
       id: 3,
       name: "Hello Gulabjal",
       price: "INR. 149/-",
       desc: "Natural & Premium Rose Water for Skin",
-      image: "/gulabjal.jpg",
+      image: "/gulabjal.jpeg",
     },
     {
       id: 4,
       name: "Charcoal Facewash",
       price: "INR. 199/-",
       desc: "Removes Dirt, Deep Cleans & Refreshes",
-      image: "/facewash.jpg",
+      image: "/haldi.jpeg",
     },
     {
       id: 3,
       name: "Hello Gulabjal",
       price: "INR. 149/-",
       desc: "Natural & Premium Rose Water for Skin",
-      image: "/gulabjal.jpg",
+      image: "/gulabjal.jpeg",
     },
     {
       id: 4,
       name: "Charcoal Facewash",
       price: "INR. 199/-",
       desc: "Removes Dirt, Deep Cleans & Refreshes",
-      image: "/facewash.jpg",
+      image: "/aloevera.jpeg",
     },
   ];
 
@@ -51,19 +51,19 @@ export default function NewlyLaunched() {
     <section className="px-8 py-12">
       <h2 className="text-4xl font-bold text-center mb-2">Newly Launched</h2>
       <p className="text-center text-gray-500 mb-8">
-        At <span className="text-[#03045E] font-semibold">ZOIC</span> it’s all about continuous innovations.
+        At <span className="text-[#048DB7] font-semibold">ZOIC</span> it’s all about continuous innovations.
       </p>
 
-      <div className="flex overflow-x-auto space-x-10 ml-10 scrollbar-hide">
+      <div className="flex overflow-x-auto space-x-10 ml-10 scrollbar-thin">
         {products.map((p) => (
           <div
             key={p.id}
-            className=" h-[60vh] w-[300px] flex-shrink-0 bg-white shadow-xl border border-gray-200 rounded-t-[50%]"
+            className=" h-[60vh] w-[25vw] shrink-0 bg-white shadow-xl border border-gray-200 rounded-t-[50%]"
           >
             <div
   className="h-[70%] w-full bg-cover bg-center  overflow-hidden rounded-t-[50%]"
   style={{
-    backgroundImage: `url(/product.png)`,
+    backgroundImage: `url(${p.image})`,
   }}
 ></div>
             <div className="p-4">
